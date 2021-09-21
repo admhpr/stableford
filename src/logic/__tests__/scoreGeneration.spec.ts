22494d7b3e9c327cb06e15a6d0ac88418fd472f4
adam <adam@harpur.io>
2021-09-21 16:19:38 +0100

    chore: setup adjust par based handicap

--- src/logic/__tests__/scoreGeneration.spec.ts
+++ src/logic/__tests__/scoreGeneration.spec.ts
@@ -1,9 +1,8 @@
-import { HoleContext, PotentialHolesPlayed } from "../types"
-
-function play18(course){}
-function play9(course){}
+function play18(course) {}
+function play9(course) {}
 
 describe(`score generation`, () => {
+  describe(`adjustParBasedOnHandicap`, () => {})
   it(`calculate scores for players on 18 holes`, () => {})
   it(`calculate scores for players on 9 holes`, () => {})
 })
--- src/logic/scoreGeneration.ts
+++ src/logic/scoreGeneration.ts
@@ -1,4 +1,33 @@
-import { HoleContext, PlayerProfile } from "./types"
+import {
+  HoleContext,
+  PlayerProfile,
+  PotentialHolePars,
+  PotentialStrokeIndexes,
+  PotentialHolesPlayed,
+} from "./types"
+
+export function adjustParBasedOnHandicap({
+  par,
+  strokeIndex,
+  handicap,
+  holesInPlay,
+}): {
+  handicap: number
+  par: PotentialHolePars
+  strokeIndex: PotentialStrokeIndexes
+  holesInPlay: PotentialHolesPlayed
+} {
+  let extraStrokes = 0
+  const additionalStrokesAwardedAfterExtra = handicap - holesInPlay
+  if (additionalStrokesAwardedAfterExtra >= 0) {
+    extraStrokes += 1
+    const addAnotherStroke = additionalStrokesAwardedAfterExtra - strokeIndex
+    if (addAnotherStroke) {
+      extraStrokes += 1
+    }
+  }
+  return extraStrokes + par
+}
 
 function calculateStableFordScoreForHole(
   player: PlayerProfile,
