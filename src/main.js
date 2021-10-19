5f88fb9f9e76a56581950eb79243419d92a5af59
adam <adam@harpur.io>
2021-10-19 17:06:14 +0100

    chore: update oruga style config

--- src/main.js
+++ src/main.js
@@ -4,4 +4,46 @@ import "@oruga-ui/oruga-next/dist/oruga.css"
 import "./assets/style.css"
 import App from "./App.vue"
 
-createApp(App).use(Oruga).mount("#app")
+createApp(App)
+  .use(Oruga, {
+    button: {
+      rootClass: "btn",
+      variantClass: "btn-",
+      roundedClass: "btn-rounded",
+      outlinedClass: "btn-outlined-",
+      disabledClass: "btn-disabled",
+    },
+    field: {
+      rootClass: "field",
+      labelClass: "field-label",
+      messageClass: "text-xs italic",
+      variantClass: "field-",
+    },
+    input: {
+      inputClass: "input",
+      roundedClass: "rounded",
+      variantClass: "input-",
+      iconRightClass: "input-icon-right",
+    },
+    icon: {
+      variantClass: "input-icon-",
+    },
+    checkbox: {
+      checkClass: "checkbox",
+      checkCheckedClass: "checkbox-checked",
+      labelClass: "checkbox-label",
+    },
+    dropdown: {
+      rootClass: "dropdown",
+      menuClass: "dropdown-menu",
+      itemClass: "dropdown-item",
+      itemActiveClass: "dropdown-item-active",
+    },
+    steps: {
+      itemHeaderActiveClass: "steps-nav-item-active",
+      itemHeaderPreviousClass: "steps-nav-item-previous",
+      stepMarkerClass: "step-marker",
+      stepDividerClass: "step-divider",
+    },
+  })
+  .mount("#app")
