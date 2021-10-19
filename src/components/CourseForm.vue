2b28d3054fb6206082c90a1fca0bd47e6feb5e03
adam <adam@harpur.io>
2021-10-19 16:32:32 +0100

    chore: setup initial components

---
+++ src/components/CourseForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+
+function onButtonClick() {
+  count.value += 1
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="onButtonClick">count is: {{ count }}</button>
+</template>
+
+<style scoped></style>
