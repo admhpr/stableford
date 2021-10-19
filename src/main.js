3d3874c2766707b667737093387f14e5631275db
adam <adam@harpur.io>
2021-10-19 16:58:48 +0100

    style: setup tailwind

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue"
 import Oruga from "@oruga-ui/oruga-next"
 import "@oruga-ui/oruga-next/dist/oruga.css"
-
+import "./assets/style.css"
 import App from "./App.vue"
 
 createApp(App).use(Oruga).mount("#app")
