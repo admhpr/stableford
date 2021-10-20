4b5c891c928ee58a305b78bef91bbc55180b9413
adam <adam@harpur.io>
2021-10-20 07:40:11 +0100

    style: add oruga full style sheet

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
 import Oruga from "@oruga-ui/oruga-next"
-import "@oruga-ui/oruga-next/dist/oruga.css"
+import "@oruga-ui/oruga-next/dist/oruga-full.css"
 import App from "./App.vue"
 
 createApp(App)
